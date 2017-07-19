43f4dd919759ee9e850b70586ec1b22db8c7e21a
PulsarFox <pulsarfox@PulsarFoxs-Mac.local>
2017-07-20 00:36:49 +0200

    ajout scroll interactif page

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <div class="titles">
         <a href="index.php" class="link_homepage">
             <h1 class="titre">Chiabrena</h1>
+        </a>
         <?php
            if ($page == "index")
                 $page = "Accueil";
@@ -9,6 +10,18 @@
                 $page = ucfirst($page);
             echo '<h2 class="undertitle">'.$page.'</h2>';
         ?>
-        </a>
+    </div>
+    <div class="menu">
+        <a class="menu_title home" href="#">Accueil</a>
+        <span class="test"></span>
+        <a class="menu_title editor" href="#">Editeur</a>
+        <a class="menu_title gallerie" href="#">Gallerie</a>
+        <?php
+            if ($_SESSION['connected'])
+                echo '<a class="menu_title myaccount" href="#">Accname</a>';
+            else
+                echo '<a class="menu_title login" href="#">Log in</a>
+                    <a class="menu_title signin" href="#">Sign in</a>';
+        ?>
     </div>
 </div>
--- index.php
+++ index.php
@@ -20,7 +20,20 @@
             <img class="presentation_image_blur" src="images/hide_face.jpg" alt="" />
             <img class="presentation_image" src="images/hide_face.jpg" alt="" />
         </div>
+        <div id="desc_text" class="description_text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit urna, auctor eu congue quis, pulvinar a quam. Morbi congue dui diam, sit amet blandit justo tristique in. Vivamus congue auctor felis, interdum tincidunt purus convallis at. Nulla facilisi. Quisque mollis at odio sed gravida. Aliquam diam odio, laoreet sit amet ornare pretium, fermentum a ante. Aenean sagittis consequat massa non luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque tincidunt, lectus vel condimentum lacinia, nunc odio tristique magna, sed congue massa risus nec lorem.
 
+Curabitur convallis vestibulum enim. Mauris ut magna at sapien faucibus ornare eget id urna. Vestibulum viverra pharetra congue. Etiam eget massa orci. Phasellus id lacinia lorem. Vivamus id eros sagittis massa auctor suscipit ut eu ex. Pellentesque aliquam convallis sem sit amet convallis.</p>
+        </div>
+        <script>
+            var test = document.getElementById('desc_text');
+            var testscroll;
+            window.addEventListener('scroll', function(){
+                testscroll = document.body.scrollTop;
+                console.log(testscroll);
+                test.style.opacity = (testscroll) / 100;
+            });
+        </script>
     </div>
 </body>
 </html>
